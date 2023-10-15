46a47dfbb33076d71fa81234fe4d7a6b1c15b3bd
DESKTOP-JQ1565L\mail2 <mail2dj12@gmail.com>
2023-10-15 10:27:40 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
